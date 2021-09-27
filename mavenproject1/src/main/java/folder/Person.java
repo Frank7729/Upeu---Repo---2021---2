a2a9e8bf99a00c08aa1253fd9c2e435345827704
Frank7729 <82120628+Frank7729@users.noreply.github.com>
2021-09-27 18:06:41 -0500

    Ejemplo POO

---
+++ mavenproject1/src/main/java/folder/Person.java
@@ -0,0 +1,18 @@
+package folder;
+
+public class Person {
+
+    private String name;
+    
+    public void laugth(){
+        System.out.println(getName()+" jejeje");
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public  String getName(){
+        return name;
+    }
+}
