3d09dafcd5ceb1102844550ed1c98e292d90c250
Frank7729 <frankchunca@gmail.com>
2021-09-28 08:04:13 -0500

    Actualizado

---
+++ Proyecto Prueba/src/name/My_name_is.java
@@ -0,0 +1,11 @@
+package name;
+
+public class My_name_is {
+    public static void Name(){
+        System.out.println("Si Responde"); 
+    } 
+    
+    public static void main(String [] args){
+        Name();
+    }
+}
