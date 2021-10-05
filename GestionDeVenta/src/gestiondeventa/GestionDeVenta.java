d374c46ec492a69be8ed44beb09a18079c727f31
Frank7729 <frankchunca@gmail.com>
2021-10-05 15:55:20 -0500

    Gestion de Venta.

---
+++ GestionDeVenta/src/gestiondeventa/GestionDeVenta.java
@@ -0,0 +1,12 @@
+package gestiondeventa;
+
+import Vista.Login;
+
+public class GestionDeVenta {
+
+    public static void main(String[] args) {
+        Login lg = new Login();
+        lg.setVisible(true);
+    }
+    
+}
